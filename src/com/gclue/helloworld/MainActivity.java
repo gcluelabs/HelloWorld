2ab4b828210bbc0435d4402bc0ec38042b4e91c7
hoshit <hoshi@gclue.jp>
2012-12-18 10:13:14 +0900

    初回コミット

---
+++ src/com/gclue/helloworld/MainActivity.java
@@ -0,0 +1,24 @@
+package com.gclue.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+//import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+//		setContentView( R.layout.activity_main );
+		TextView txView = new TextView(this);
+		txView.setText("Hello Android!!");
+		setContentView(txView);
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu( Menu menu ) {
+//		getMenuInflater().inflate( R.menu.activity_main, menu );
+//		return true;
+//	}
+}
